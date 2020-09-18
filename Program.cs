cd079e3d205d19e510535e13ed6a032cc22c4d9c
Felipe França <contatofelipefranca12@gmail.com>
2020-09-17 21:13:58 -0300

    Exibe nome usuário

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace OlaUsuario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.Write("Digite seu nome: ");
+            string nome = Console.ReadLine();
+            Console.WriteLine($"Olá, {nome}");
+            Console.ReadKey();
+        }
+    }
+}
